8f8966fd86c33d676a67d5b109817c8d2b1c1e7b
AlexandreSampaioGuimaraesRibeiro <lexandre.guimaraes@gmail.com>
2026-08-26 19:15:58 -0300

    finalizando-APIClimaTempo

--- ClimaAPI/src/main/java/com/example/ClimaAPI/controller/Controller.java
+++ ClimaAPI/src/main/java/com/example/ClimaAPI/controller/Controller.java
@@ -2,25 +2,26 @@ package com.example.ClimaAPI.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.ClimaAPI.service.Service;
 
 @RestController
-public class Cotroller {
+public class Controller {
     
     Service service = new Service();
 
-    @GetMapping("/clima")
-    public String consultarClima(){
-        return service.consultarClima();
-    }
 
-    @GetMapping("/clima/{cidade}")//http://localhosst:8080/modelos/59
-    public String preverTempoCidade(@PathVariable String cidade) {
-        return service.preverTempoCidade(cidade);
-    }
-    
+    @GetMapping("/clima/{latitude}/{longitude}")//http://localhosst:8080/modelos/59
+    public String consultarCidade(
+        @PathVariable double latitude,
+        @PathVariable double longitude,
+        @RequestParam(required = false) String hourly,
+        @RequestParam(required = false) String daily) {
+    return service.consultarCidade(latitude, longitude, hourly, daily);
+}
+    /*
     @GetMapping("anos/{marca}/{modelos}")
     public String consultarAnos(@PathVariable int marca, @PathVariable int modelo) {
         return service.consultarAnos(marca,modelo);
@@ -29,5 +30,5 @@ public class Cotroller {
     @GetMapping("/valor/{marca}/{modelo}/{ano}")
     public String consultarValor(@PathVariable int marca, @PathVariable int modelo, @PathVariable int ano) {
         return service.consultarValor(marca,modelo,ano);
-    }
+    }*/
 }
--- ClimaAPI/src/main/java/com/example/ClimaAPI/service/Service.java
+++ ClimaAPI/src/main/java/com/example/ClimaAPI/service/Service.java
@@ -2,38 +2,48 @@ package com.example.ClimaAPI.service;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 public class Service {
-
-    private static final String BASE_URL = "https://api.open-meteo.com/v1";
+    //https://api.open-meteo.com/v1/forecast?latitude=-19.9208&longitude=-43.9378&daily=
+    //temperature_2m_max,temperature_2m_min,weather_code&hourly=temperature_2m,relative_humidity_2m,
+    //weather_code,wind_speed_10m,wind_direction_10m,apparent_temperature,dew_point_2m&timezone=America
+    //%2FSao_Paulo&forecast_days=1
+    private static final String BASE_URL = "https://api.open-meteo.com/v1/forecast";
 
     private String consultarURL(String apiUrl){
-        String dados = "";
-        RestTemplate restTemplate = new RestTemplate();
+    RestTemplate restTemplate = new RestTemplate();
+    try {
         ResponseEntity<String> responseEntity = restTemplate.getForEntity(apiUrl, String.class);
-        if (responseEntity.getStatusCode().is2xxSuccessful()) {
-            dados = responseEntity.getBody();
-        } else {
-            dados = "falha ao obter dados. Código de status:" + responseEntity.getStatusCode();
+        return responseEntity.getBody();
+    } catch (Exception e) {
+        return "Erro na requisição: " + e.getMessage();
+    }
+}
+
+
+    public String consultarCidade(double latitude, double longitude, String hourly, String daily){
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(BASE_URL)
+                .queryParam("latitude", latitude)
+                .queryParam("longitude", longitude)
+                .queryParam("timezone", "America/Sao_Paulo")
+                .queryParam("forecast_days", 1);
+
+        if (daily != null && !daily.isEmpty()) {
+            builder.queryParam("daily", daily);
         }
-        
-        
-        return dados;
-    }
+        if (hourly != null && !hourly.isEmpty()) {
+            builder.queryParam("hourly", hourly);
+        }
 
-    public String consultarClima(){
-        return consultarURL(BASE_URL+"/cars/brands");
+        return consultarURL(builder.toUriString());
     }
-
-    public String preverTempoCidade(String cidade){
-        return consultarURL(BASE_URL+"/cars/brands/"+cidade+"/models");
-    }
-
+/*
     public String consultarAnos(int marca, int modelo){
         return consultarURL(BASE_URL+"/cars/brands/"+marca+"modelos"+modelo+"/years");
     }
 
     public String consultarValor(int marca, int modelo, int ano){
         return consultarURL(BASE_URL+"/cars/brands/"+marca+"modelos"+modelo+"/years"+ano);
-    }
+    }*/
 }
