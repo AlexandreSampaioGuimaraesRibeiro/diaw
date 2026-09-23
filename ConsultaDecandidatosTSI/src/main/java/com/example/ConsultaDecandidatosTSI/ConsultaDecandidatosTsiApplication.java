35c8fafcc1cdb08a6cc84daa0445d9d3815e4b99
Alexandre Sampaio Guimarães Ribeiro <1664396@sga.pucminas.br>
2026-09-23 11:00:07 -0300

    ConsultaDeCanditadosTSI

---
+++ ConsultaDecandidatosTSI/src/main/java/com/example/ConsultaDecandidatosTSI/ConsultaDecandidatosTsiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ConsultaDecandidatosTSI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaDecandidatosTsiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaDecandidatosTsiApplication.class, args);
+	}
+
+}
---
+++ ConsultaDecandidatosTSI/src/test/java/com/example/ConsultaDecandidatosTSI/ConsultaDecandidatosTsiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ConsultaDecandidatosTSI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaDecandidatosTsiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
